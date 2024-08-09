627460f3f839b60f161cf3c507e00bbd8e916e71
bboodd <gkrgus9@gmail.com>
2024-08-09 17:43:31 +0900

    메인 컨트롤러 리팩토링(log, switch 제거)

--- src/main/java/com/study/controller/BoardControllerServlet.java
+++ src/main/java/com/study/controller/BoardControllerServlet.java
@@ -19,46 +19,40 @@ public class BoardControllerServlet extends HttpServlet {
     HashMap<String, HttpService> commandMap = new HashMap<>();
 
     public void init(){
-        commandMap.put("GET:list", new GetListService() );
-        commandMap.put("GET:read", new GetReadService() );
-        commandMap.put("GET:insert", new GetInsertService() );
-        commandMap.put("POST:insert", new PostInsertService() );
-        commandMap.put("GET:update", new WriteService() );
-        commandMap.put("PUT:update", new WriteService() );
-        commandMap.put("GET:delete", new DeleteService() );
-        commandMap.put("POST:search", new PostSearchService() );
-        commandMap.put("POST:comment", new PostCommentService() );
+        commandMap.put("GET/list.do", new GetListService() );
+        commandMap.put("GET/read.do", new GetReadService() );
+        commandMap.put("GET/insert.do", new GetInsertService() );
+        commandMap.put("POST/insert.do", new PostInsertService() );
+        commandMap.put("GET/update.do", new WriteService() );
+        commandMap.put("PUT/update.do", new WriteService() );
+        commandMap.put("GET/delete.do", new DeleteService() );
+        commandMap.put("POST/search.do", new PostSearchService() );
+        commandMap.put("POST/comment.do", new PostCommentService() );
         commandMap.put("Unknown", new UnknownService() );
     }
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        log.trace("service(req, res) invoked.");
-
         HttpService targetService = findTargetService(request);
 
         //비즈니스 로직 수행 결과 데이터를 공유데이터영역인 "Application Scope"(accessed by ServletContext)에 바인딩하는 방법도 있음.
         String view = targetService.doService(request, response);
 
-        //TODO: view 문자열 분석해서 dispatch 혹은 redirect
-        String flagWord = view.substring(0, 9);
-        String viewWord = view.substring(9, view.length());
+        String pathHead = view.substring(0, 9);
+        String viewPath = view.substring(9, view.length());
 
         log.info("넘어온 view 문자열은: {}", view);
-        log.info("flagWord: {}", flagWord);
-        log.info("viewWord: {}", viewWord);
+        log.info("flagWord: {}", pathHead);
+        log.info("viewWord: {}", viewPath);
 
-        if(flagWord.equals("dispatch:")){
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewWord);
+        if(pathHead.equals("dispatch:")){
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
             requestDispatcher.forward(request, response);
         } else{
-            response.sendRedirect(viewWord);
+            response.sendRedirect(viewPath);
         }
     }
 
     private HttpService findTargetService(HttpServletRequest  request){
-        //TODO: method 및 requestUri 를 분석해서 담당서비스를 찾아서 리턴
-
         //method 및 uri분석
         String method = request.getMethod();
         String requestUri = request.getRequestURI();
@@ -66,49 +60,24 @@ public class BoardControllerServlet extends HttpServlet {
         String requestUrl = request.getRequestURL().toString();
         String queryString = request.getQueryString();
 
+        /**
+         * String command = requestUri.substring(contextPath.length());
+         *   + command는 contextPath가 그냥 루트일 경우 URI와 동일하게 나온다.
+         *   + substring으로 uri에서 컨텍스트 패스를 제외한 문자열을 돌려준다.
+         *   + contextPath가 /(루트)일 경우 빈 문자열이라 substring 필요없음
+         */
+
         log.info("\t + 0. contextPath : {}", contextPath); // /maven01 or /myapp
-
-        // + contextPath가 /(루트)일 경우 빈 문자열이라 substring 필요없음
-
         log.info("\t + 1. requestUri : {}", requestUri); // /*.do
         log.info("\t + 2. requestUrl : {}", requestUrl); // http://localhost:8080/*.do
         log.info("\t + 3. method : {}", method); // GET
+        log.info("\t + 4. queryString : {}", queryString); // ?...&..
 
-        // substring으로 uri에서 컨텍스트 패스를 제외한 문자열을 돌려준다.
-        // String command = requestUri.substring(contextPath.length());
+        HttpService service = commandMap.get(method+requestUri);
 
-        String command = requestUri;
-        log.info("\t + 4. command : {}", command); // /*.do
-
-        // command는 contextPath가 그냥 루트일 경우 URI와 동일하게 나온다.
-
-        log.info("\t + 5. queryString : {}", queryString); //
-
-        HttpService service =
-
-        switch (method) { //담당 서비스를 찾는 로직
-            case "GET" -> switch (requestUri) {
-                    case "/list.do" -> commandMap.get("GET:list");
-                    case "/read.do" -> commandMap.get("GET:read");
-                    case "/insert.do" -> commandMap.get("GET:insert");
-                    case "/update.do" -> commandMap.get("GET:update");
-                case "/delete.do"-> commandMap.get("GET:delete");
-                    default -> commandMap.get("Unknown"); //오류페이지 오출
-                };
-            case "POST" ->
-                switch (requestUri) {
-                    case "/insert.do"-> commandMap.get("POST:insert");
-                    case "/search.do" -> commandMap.get("POST:search");
-                    case "/comment.do" -> commandMap.get("POST:comment");
-                    default -> commandMap.get("Unknown"); //오류페이지 오출
-                };
-            case "PUT"->
-                switch (requestUri) {
-                    case "/update.do"-> commandMap.get("PUT:update");
-                    default -> commandMap.get("Unknown"); //오류페이지 오출
-                };
-            default -> commandMap.get("Unknown"); //오류페이지 오출
-        };
+        if(service == null){
+            service = commandMap.get("Unknown");
+        }
 
         return service;
     }
